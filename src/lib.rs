949a4260fbe11626d7d0e1117513bc6de4aff674
Fabien-B <fabien.bonneval@gmail.com>
2024-03-14 15:17:34 +0100

    Initiate connection to new peers, handle TCP socket closing

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ use crossbeam::select;
 use ivy_messages::{IvyMsg, parse_udp_announce};
 use regex::Regex;
 //use std::time::Duration;
-use socket2::{Socket, Domain, Type, Protocol};
+use socket2::{Domain, Protocol, Socket, Type};
 use ivyerror::IvyError;
 //use crate::ivy_messages::IvyMsg;
 // use peer::Peer;
@@ -64,6 +64,7 @@ struct PeerData {
 }
 
 struct IvyPrivate {
+    appname: String,
     peers: HashMap<u32, Peer>,
     next_thread_id: AtomicCell<u32>,
     client_connected_cb: Option<Box<dyn Fn() + Send + Sync>>,
@@ -75,8 +76,9 @@ struct IvyPrivate {
 }
 
 impl IvyPrivate {
-    fn new() -> Self {
+    fn new(appname: String) -> Self {
         IvyPrivate {
+            appname,
             peers: HashMap::new(),
             next_thread_id: AtomicCell::new(2),
             client_connected_cb: None,
@@ -90,7 +92,6 @@ impl IvyPrivate {
 }
 
 pub struct IvyBus {
-    pub appname: String,
     private: Arc<RwLock<IvyPrivate>>,
     snd: Option<Sender<Command>>,
     next_sub_id: AtomicCell<u32>,
@@ -114,8 +115,7 @@ impl IvyBus {
 
     pub fn new(appname: &str) -> Self {
         IvyBus {
-            appname: appname.into(),
-            private: Arc::new(RwLock::new(IvyPrivate::new())),
+            private: Arc::new(RwLock::new(IvyPrivate::new(appname.into()))),
             snd: None,
             next_sub_id: AtomicCell::new(0)
         }
@@ -128,9 +128,13 @@ impl IvyBus {
     }
 
     pub fn subscribe(&self, regex: &str, cb: Box<dyn Fn(&Vec<String>) + Send + Sync>) {
+
+        let mut bp = self.private.write().unwrap();
+        let sub_id = self.next_sub_id.fetch_add(1);
+        bp.subscriptions.insert(sub_id, (regex.into(), cb));
+
+        // if ivy loop is running, send a message
         if let Some(snd) = &self.snd {
-            let sub_id = self.next_sub_id.fetch_add(1);
-            self.private.write().unwrap().subscriptions.insert(sub_id, (regex.into(), cb));
             let cmd = Command::Sub(sub_id, regex.into());
             let _ = snd.send(cmd);
         }
@@ -154,18 +158,30 @@ impl IvyBus {
 
     }
 
+    fn make_udp_socket(domain: &str) -> Result<(Socket, SocketAddr), IvyError> {
+        let udp_socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+        let address: SocketAddr = domain.parse().unwrap();
+        udp_socket.set_reuse_address(true)?;
+        udp_socket.set_broadcast(true)?;
+        let _ = udp_socket.set_read_timeout(Some(Duration::from_millis(10)));
+        udp_socket.bind(&address.into())?;
+        Ok((udp_socket, address))
+    }
+
     pub fn start_ivy_loop(&mut self, domain: &str) -> Result<(), IvyError> {
 
 
 
         // create TCPListener and bind it 
-        //let listener = TcpListener::bind("0.0.0.0:0")?;
-        let listener = TcpListener::bind("0.0.0.0:8888")?;
+        let listener = TcpListener::bind("0.0.0.0:0")?;
         let port = listener.local_addr()?.port();
         self.private.write().unwrap().local_port = port;
 
         // channel for new TCP connections
         let (snd_tcp, rcv_tcp) = unbounded::<(TcpStream, SocketAddr)>();
+
+        // channel for new UDP client
+        let (snd_udp, rcv_udp) = unbounded::<(SocketAddr, String)>();
 
         // channels for thread terminaisons
         let (snd_thd_terminated, rcv_thd_terminated) = unbounded::<u32>();
@@ -184,29 +200,17 @@ impl IvyBus {
         //TODO add thread handle to hashmap
         //self.private.write().unwrap().join_handles.insert(TCPLISTENER_THD_ID, tcplistener_handle);
 
-
-
-        // create UDP socket
-        let udp_socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
-        let address: SocketAddr = domain.parse().unwrap();
-        udp_socket.set_reuse_address(true)?;
-        udp_socket.set_broadcast(true)?;
-        //udp_socket.set_nonblocking(true)?;
-        let _ = udp_socket.set_read_timeout(Some(Duration::from_millis(10)));
-        udp_socket.bind(&address.into())?;
-
-        // Send UDP annoucement
-        let watcher_id = format!("{}_{}", self.appname, port);
-        // <protocol version> <TCP port> <watcherId> <application name>
-        let announce = format!("{PROTOCOL_VERSION} {port} {watcher_id} {}\n", self.appname);
-        let _ = udp_socket.send_to(announce.as_bytes(), &address.into());
-
-        // channels for thread terminaisons
+        // channel for thread terminaisons
         let udp_snd_term = snd_thd_terminated.clone();
+        // terminate flag
         let udp_term_flag = self.private.read().unwrap().should_terminate.clone();
+        // domain and appname to pass to UDP thread
+        let appname_udp = self.private.read().unwrap().appname.clone();
+        let domain_udp = domain.to_string();
         // UDP Listener for new peers
-        let udplistener_handle = thread::spawn(move || Self::udp_listener(udp_socket, udp_snd_term, udp_term_flag, watcher_id));
+        let udplistener_handle = thread::spawn(move || Self::udp_listener(domain_udp, appname_udp, port, udp_snd_term, udp_term_flag, snd_udp));
         self.private.write().unwrap().join_handles.insert(UDPLISTENER_THD_ID, udplistener_handle);
+
 
         // main Ivy loop
         // channels for commands (client thread to Ivy thread)
@@ -217,6 +221,7 @@ impl IvyBus {
             Self::ivy_loop(bus_private,
                 rcv_tcp,
                 rcv_cmd,
+                rcv_udp,
                 snd_thd_terminated,
                 rcv_thd_terminated)
         );
@@ -229,10 +234,21 @@ impl IvyBus {
 
 
     fn udp_listener(
-            udp_socket: Socket,
-            udp_snd_term: Sender<u32>,
+            domain: String,
+            appname: String,
+            port: u16,
+            snd_term: Sender<u32>,
             term: Arc<AtomicBool>,
-            my_watcher_id: String) {
+            snd_udp: Sender<(SocketAddr, String)>) {
+
+        let (udp_socket, address) = Self::make_udp_socket(&domain).unwrap();
+        // Send UDP annoucement
+        let my_watcher_id = format!("{}_{}", appname, port);
+        // <protocol version> <TCP port> <watcherId> <application name>
+        let announce = format!("{PROTOCOL_VERSION} {port} {my_watcher_id} {}\n", appname);
+        let _ = udp_socket.send_to(announce.as_bytes(), &address.into());
+
+
         let mut _buf = [MaybeUninit::uninit();1024];
         
         loop
@@ -258,8 +274,14 @@ impl IvyBus {
                     }
                     
                     println!("announce: {watcher_id} / {my_watcher_id}");
-                    // TODO send (port, peer_name) to ivy thread
-                    (port, peer_name);
+                    // send (port, peer_name) to ivy thread
+                    if let Some(addr) = src.as_socket() {
+                        let ip = addr.ip();
+                        let addr = SocketAddr::new(ip, port);
+                        let _ = snd_udp.send((addr, peer_name));
+                    }
+                    
+                    
                 } else {
                     
                     println!("{parsed:?}");
@@ -270,13 +292,16 @@ impl IvyBus {
                 break;
             }
         }
-        let _ = udp_snd_term.send(UDPLISTENER_THD_ID);
+        let _ = snd_term.send(UDPLISTENER_THD_ID);
     }
 
-    fn ivy_loop(bus_private: Arc<RwLock<IvyPrivate>>, rcv_tcp: Receiver<(TcpStream, SocketAddr)>, rcv_cmd: Receiver<Command>,
+    fn ivy_loop(bus_private: Arc<RwLock<IvyPrivate>>, rcv_tcp: Receiver<(TcpStream, SocketAddr)>, rcv_cmd: Receiver<Command>, rcv_udp: Receiver<(SocketAddr, String)>,
     snd_thd_terminated: Sender<u32>, rcv_thd_terminated: Receiver<u32>) {
 
         let (sen_peer, rcv_peer) = unbounded::<(u32, IvyMsg)>();
+
+
+        
 
         loop {
             select! {
@@ -298,6 +323,45 @@ impl IvyBus {
                         Self::handle_command(cmd, &bus_private);
                     }
                 },
+                recv(rcv_udp) -> udp_msg => {
+                    if let Ok((addr, peer_name)) = udp_msg {
+                        let ss = TcpStream::connect(addr);
+                        if let Ok(mut stream) = ss {
+                            let stream_tcp_read = stream.try_clone().unwrap();
+                            let stream_peer = stream.try_clone().unwrap();
+                            let mut bp = bus_private.write().unwrap();
+
+                            let mut peer = Peer::new(stream_peer);
+                            peer.name = peer_name;
+                            let peer_id = bp.next_thread_id.fetch_add(1);
+
+                            let pd = PeerData{
+                                socket: stream_tcp_read,
+                                snd_ivymsg: sen_peer.clone(),
+                                peer_id,
+                                term: peer.should_terminate.clone(),
+                                snd_thd_terminated: snd_thd_terminated.clone(),
+                            };
+
+                            let handle = thread::spawn(move || Self::tcp_read(pd));
+                            bp.join_handles.insert(peer_id, handle);
+                            bp.peers.insert(peer_id, peer);
+                            
+                            let msg = IvyMsg::PeerId(bp.local_port, bp.appname.clone());
+                            let buf = msg.to_ascii();
+                            let _ = stream.write(&buf);
+                            // send Initial Subscriptions
+                            for (sub_id, (regex, _)) in bp.subscriptions.iter() {
+                                let msg = IvyMsg::Sub(*sub_id, regex.clone());
+                                let buf = msg.to_ascii();
+                                let _ = stream.write(&buf);
+                            }
+                            // send end of initial subsciption
+                            let buf = IvyMsg::EndSub.to_ascii();
+                            let _ = stream.write(&buf);
+                    }
+                    }
+                }
                 // notification thread termination
                 recv(rcv_thd_terminated) -> msg => {
                     if let Ok(thread_id) = msg {
@@ -512,11 +576,16 @@ impl IvyBus {
     /// 
     /// Report terminaison via the `data.snd_thd_terminated` channel
     fn tcp_read(mut data: PeerData) {
+        println!("new peer thread");
         let mut buf = vec![0; 1024];
         let _ = data.socket.set_read_timeout(Some(Duration::from_millis(10)));
         loop {
             match data.socket.read(&mut buf) {
                 Ok(n) => {
+                    if n == 0 {
+                        // socket closed
+                        break;
+                    }
                     let lines = buf[0..n]
                     .split(|c| *c==b'\n')
                     .filter(|buf| buf.len() > 0);
@@ -527,8 +596,17 @@ impl IvyBus {
                         }
                     }
                 },
-                Err(_) => (),
+                Err(e) => {
+                    match e.kind() {
+                        std::io::ErrorKind::WouldBlock => {},
+                        _ => {
+                            println!("tcp error: {}", e.kind());
+                            break;
+                        },
+                    }
+                },
             }
+
             if data.term.load(Ordering::Acquire) {
                 break;
             }
@@ -544,10 +622,9 @@ impl IvyBus {
 
 impl fmt::Debug for IvyBus {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{{\"{}\"", self.appname)?;
-        //let p = self.private.read().unwrap();
+        let p = self.private.read().unwrap();
         f.debug_struct("IvyBus")
-            .field("appname", &self.appname)
+            .field("appname", &p.appname)
             //.field("private", &format_args!("{:?}", *p))
             .finish()
     }
